35b4cef0ddb52e345423fad2748911f8cad8fe7e
TheLastEmanator <38412253+UpInStratosphere@users.noreply.github.com>
2022-08-14 18:44:31 -0400

    Add function to interprets build-in commands

--- tsh.c
+++ tsh.c
@@ -227,13 +227,21 @@ int parseline(const char *cmdline, char **argv) {
     return bg;
 }
 
-/* 
- * builtin_cmd - If the user has typed a built-in command then execute
- *    it immediately.  
- */
-int builtin_cmd(char **argv) 
-{
-    return 0;     /* not a builtin command */
+/* builtin_cmd - If the user has typed a built-in command then execute it immediately. */
+
+int builtin_cmd(char **argv) {
+    if (!strcmp(argv[0], "quit")){
+        exit(0);
+    }
+    if (!strcmp(argv[0], "&")){
+        return 1;
+    }
+    if (!strcmp(argv[0], "bg") || !strcmp(argv[0], "fg")){
+        do_bgfg(argv);
+        return 1;
+    }
+    
+    return 0;    
 }
 
 /* 
